446a74361b7efa7d1152468a43c132b688caf6c6
parjolandrei <parjol.andrei@yahoo.com>
2020-03-08 14:24:42 +0200

    Updates

---
+++ precache-manifest.1c69d1f495bef2a754ddedbc402b8f8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8738c65d4ffe30a0ceb26384922a4f2a",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "fdbc168f09c6fa7ea47a",
+    "url": "/game-of-life/static/css/main.e9ee07a3.chunk.css"
+  },
+  {
+    "revision": "03cd35cf34e3a5fcc20a",
+    "url": "/game-of-life/static/js/2.7a169bf3.chunk.js"
+  },
+  {
+    "revision": "95fe7b47190d8ffb46ace11b00922862",
+    "url": "/game-of-life/static/js/2.7a169bf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdbc168f09c6fa7ea47a",
+    "url": "/game-of-life/static/js/main.de8c3ac6.chunk.js"
+  },
+  {
+    "revision": "41d353de8e24f2a7af0a",
+    "url": "/game-of-life/static/js/runtime-main.37003e51.js"
+  }
+]);
